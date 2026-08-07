965c9ad7ef1853b17e6d009aae7a9d49dd1676a7
joe33143 <42575175+joe33143@users.noreply.github.com>
2026-08-07 09:51:22 +0530

    Update day_effects.py

--- day_effects.py
+++ day_effects.py
@@ -1,10 +1,9 @@
 def get_day_payload(r, g, b, pwm, clouds, base_phase_name, is_stormy=False):
-    """Applies daytime animations, using live calculated colors for weather."""
-    c = clouds / 100.0
+    c = max(clouds, 100 if is_stormy else 0) / 100.0
     
-    if clouds >= 75 and not is_stormy:
+    if (clouds >= 75 or is_stormy):
         grey = (r + g + b) // 3
-        fade = ((clouds - 75) / 25.0) * 0.7 
+        fade = 0.7 
         r = int(r + (grey - r) * fade)
         g = int(g + (grey - g) * fade)
         b = int(b + (grey - b) * fade)
@@ -18,31 +17,16 @@ def get_day_payload(r, g, b, pwm, clouds, base_phase_name, is_stormy=False):
     pal = 0
     phase_name = base_phase_name
 
-    if is_stormy:
-        # --- CALCULATED THUNDER STORM ---
-        fx = 57  
-        sx = 220  # High frequency of strikes
-        ix = 200  # High intensity flashes
-        pal = 0   
-        
-        # col1 is the FLASH (vivid, bright calculated color)
-        col1 = [min(255, int(r * 1.5)), min(255, int(g * 1.5)), min(255, int(b * 1.5)), 0]  
-        # col2 is the BACKGROUND (moody, dark calculated color)
-        col2 = [int(r * 0.3), int(g * 0.3), int(b * 0.3), 0]  
-        
-        # PWM scales naturally but won't dip below 18 if the sun is up
-        pwm = max(int(pwm * 0.8), 18) 
-        phase_name += " [THUNDER STORM ACTIVE]"
-        
-    elif clouds >= 75:
+    if is_stormy or clouds >= 75:
+        # --- CLOUDY / STORMY AURORA ---
         fx = 88
-        sx = 96
+        sx = 120 if is_stormy else 96  # slightly faster clouds during storms
         ix = 224
         pal = 0  
         col2 = [int(r * 0.8), int(g * 0.8), int(b * 0.8), 0]
         col3 = [int(min(255, r * 1.3)), int(min(255, g * 1.3)), int(min(255, b * 1.3)), 0]
         pwm = max(pwm, 18)
-        phase_name += f" [Rainy / Overcast: {clouds}%]"
+        phase_name += " [THUNDER STORM ACTIVE]" if is_stormy else f" [Rainy / Overcast: {clouds}%]"
         
     elif base_phase_name == "Low Sun / Horizon" and clouds < 30:
         fx = 88
@@ -72,4 +56,3 @@ def get_day_payload(r, g, b, pwm, clouds, base_phase_name, is_stormy=False):
         phase_name += f" [Rolling Clouds: {clouds}%]"
 
     return phase_name, col1, col2, col3, pwm, fx, sx, ix, pal
-    
